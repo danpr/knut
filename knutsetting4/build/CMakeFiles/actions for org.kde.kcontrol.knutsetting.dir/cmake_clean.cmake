1f9f9f80a407e0df639293eda4888e5cc5c151ec
Daniel Prynych <Daniel.Prynych@gmail.com>
2014-11-12 08:05:42 +0100

    init knutsetting 5

--- knutsetting4/build/CMakeFiles/actions for org.kde.kcontrol.knutsetting.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/actions for org.kde.kcontrol.knutsetting"
-  "org.kde.kcontrol.knutsetting.policy"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/actions for org.kde.kcontrol.knutsetting.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
