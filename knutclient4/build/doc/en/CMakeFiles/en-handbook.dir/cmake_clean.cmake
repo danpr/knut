0df3911467003470cee686b6d2108edd418a751b
Daniel Prynych <Daniel.Prynych@gmail.com>
2014-11-12 09:12:26 +0100

    init knutclient 2

--- knutclient4/build/doc/en/CMakeFiles/en-handbook.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/en-handbook"
-  "index.cache.bz2"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/en-handbook.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
